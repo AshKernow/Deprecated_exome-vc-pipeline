e25b6aa13c3e1d25e2984ff7e784186029f43163
Ashley Sawle <ads2202@c2b2.columbia.edu>
2014-02-03 10:53:25 -0500

    FastQC script for QC of fastq files; input is a file containing a list of files (full paths) to be QCed. Job runs as array job, one job per line of the input file.

---
+++ ExmQC.1.Run_FastQC_on_File_List.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#$ -l mem=1G,time=4:: -pe smp 4 -N FastQC -cwd
+
+while getopts f:o:s: opt; do
+  case "$opt" in
+      f) FastQFileList="$OPTARG";;
+	  o) OutDir="$OPTARG";;
+	  s) Settings="$OPTARG";;
+  esac
+done
+
+if [[ ! $FastQFileList ]];then
+	echo "Please supply a file containing a list of fastq files to QC"
+	exit 1
+fi
+
+if [[ ! $Settings ]]; then
+	Settings="/ifs/home/c2b2/af_lab/ads2202/scratch/Exome_Seq/scripts/BISR_pipeline_scripts/BISR_Exome_pipeline_settings.sh"
+fi
+
+if [[ ! $OutDir ]]; then
+	OutDir="./"
+fi
+
+. $Settings
+
+FQFil=$(tail -n+$SGE_TASK_ID $FastQFileList | head -n 1)
+echo $FQFil
+mkdir -p $OutDir
+$FASTQC -o $OutDir -j $JAVA7BIN -t 4 $FQFil
