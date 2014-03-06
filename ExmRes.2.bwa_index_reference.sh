718bccdeba5742ce2925f6b7f2bdc4d10d9d5b39
Ashley Sawle <ads2202@c2b2.columbia.edu>
2014-03-06 13:58:52 -0500

    Add hg19 resources

--- ExmRes.2.bwa_index_reference.sh
+++ ExmRes.2.bwa_index_reference.sh
@@ -3,7 +3,15 @@
 uname -a
 echo "Start $0:`date`"
 
-. /ifs/home/c2b2/af_lab/ads2202/scratch/Exome_Seq/scripts/BISR_pipeline_scripts/BISR_Exome_pipeline_settings
+. /ifs/home/c2b2/af_lab/ads2202/scratch/Exome_Seq/scripts/BISR_pipeline_scripts/BISR_Exome_pipeline_settings.sh
+echo $BWA
+echo $REF
+
+cmd="$BWA index -a bwtsw $REF"
+echo $cmd
+# $cmd
+
+. /ifs/home/c2b2/af_lab/ads2202/scratch/Exome_Seq/scripts/BISR_pipeline_scripts/BISR_Exome_pipeline_settings_hg19.sh
 echo $BWA
 echo $REF
 
