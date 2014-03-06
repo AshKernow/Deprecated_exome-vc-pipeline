aa7b9b6623b68bdb63f2d783e4ab6ddb02851ba4
Ashley Sawle <ads2202@c2b2.columbia.edu>
2014-03-06 13:59:05 -0500

    more databases

--- ExmRes.4.ANNOVAR_download_databases.sh
+++ ExmRes.4.ANNOVAR_download_databases.sh
@@ -28,3 +28,32 @@ cmd="perl annotate_variation.pl -downdb -buildver hg19 -webfrom annovar ljb2_all
 echo $cmd
 $cmd
 
+echo ""
+echo "Download: alternative allele frequency in all subjects in the NHLBI-ESP project with 6500 exomes, including the indel calls and the chrY calls "
+cmd="perl annotate_variation.pl -downdb -buildver hg19 -webfrom annovar esp6500si_all humandb/"
+echo $cmd
+$cmd
+echo ""
+echo "Download: alternative allele frequency in African Americans in the NHLBI-ESP project with 6500 exomes, including the indel calls and the chrY calls  "
+cmd="perl annotate_variation.pl -downdb -buildver hg19 -webfrom annovar esp6500si_aa humandb/"
+echo $cmd
+$cmd
+
+echo ""
+echo "Download: alternative allele frequency in all subjects in the NHLBI-ESP project with 6500 exomes, including the indel calls and the chrY calls "
+cmd="perl annotate_variation.pl -downdb -buildver hg19 -webfrom annovar esp6500si_ea humandb/"
+echo $cmd
+$cmd
+
+echo ""
+echo "Download: whole-exome SIFT scores for non-synonymous variants  "
+cmd="perl annotate_variation.pl -downdb -buildver hg19 -webfrom annovar avsift humandb/"
+echo $cmd
+$cmd
+
+echo ""
+echo "Download: conserved genomic regions by GERP++ "
+cmd="perl annotate_variation.pl -downdb -buildver hg19 -webfrom annovar gerp++elem humandb/"
+echo $cmd
+$cmd
+
